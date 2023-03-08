61ab75b113fe690b9d593ff0fd81cbd28672769f
Heechem <hichempeter@gmail.com>
2023-03-08 08:02:26 -0700

    second commit

--- Readme.md
+++ Readme.md
@@ -3,3 +3,6 @@ Struggling with the scopping of the functions
 Added some UI to visualize , will improve it at the end
 Followed a video to understand the script , but still strugguling
 Updated the UI , fully functionnal
+Logic to check if there is a winner is implemented
+Winning function implemented.
+tie logic implemented with a partial UI .
--- script.js
+++ script.js
@@ -13,7 +13,7 @@
     [0, 4, 8],
     [2, 4, 6],
   ];
-  // const arr = [['','',''], ['','',], []];
+  const winnerArr = [];
 
   const playerTurn = document.querySelector('.game_info');
   const boardGame = document.getElementById('board');
@@ -21,56 +21,48 @@
   const cellsId = document.querySelectorAll('[data-pos]');
   const player1 = { name: 'Player 1', class: 'player_one', input: 'X' };
   const player2 = { name: 'Player 2', class: 'player_two', input: 'O' };
+  const paragraphe = document.querySelector('p');
+  const WinningDisplay = document.querySelector('.winner');
+  const restartBtn = document.querySelector('.restart');
   let defaultPlayer = player1;
   playerTurn.textContent = 'player 1 start please ';
-  const currentClass = defaultPlayer ? player1.class : player2.class;
 
   // Switch the player
   function switchPlayer() {
-    defaultPlayer == player1
+    defaultPlayer === player1
       ? (defaultPlayer = player2)
       : (defaultPlayer = player1);
     playerTurn.textContent = `${defaultPlayer.name}'s turn`;
   }
 
-  // function boardGrid() {
-  //   const arr = [];
-  //   const n = 3;
-  //   const m = 3;
-  //   for (let i = 0; i < n; i++) {
-  //     arr.push(new Array(m).fill(''));
-  //   }
-
-  // function inputChoice(index, choice) {
-  //   arr.map((el) => {
-  //     if (choice <= 3) {
-  //       el.splice(index, 1, choice);
-  //     }
-  //     el.push(defaultPlayer.input);
-  //   });
-  //   console.log(arr);
-  // }
-  // inputChoice();
-  // }
-
   function handlclik() {
     boardGame.addEventListener('click', (e) => {
       const choice = e.target;
+      //adding the class to check the winner
+      const currentClass =
+        defaultPlayer === player1 ? player1.class : player2.class;
 
       if (choice.dataset) {
         cellsId.forEach((cell) => {
           if (choice.textContent == '') {
             choice.textContent = defaultPlayer.input;
-            if (defaultPlayer == player1) {
-              choice.classList.add('player_one');
+            if (defaultPlayer === player1) {
+              choice.classList.add(currentClass);
             } else {
-              choice.classList.add('player_two');
+              choice.classList.add(currentClass);
             }
             let classGame = choice.className;
             console.log(classGame);
-            if (checkwinner(cell)) {
+            checkWinner(currentClass);
+            if (endGame(currentClass)) {
               console.log('yes');
+              WinningDisplay.classList.add('show');
             }
+            if (!endGame(currentClass) && winnerArr.length === 9) {
+              WinningDisplay.classList.add('show');
+              paragraphe.innerText = `Its a tie , restart the game`;
+            }
+
             switchPlayer();
           } else {
             return;
@@ -81,62 +73,19 @@
   }
   handlclik();
 
-  function checkwinner(cell) {
+  //check the end of the game
+
+  function checkWinner(currentClass) {
+    winnerArr.push(currentClass);
+
+    console.log(winnerArr);
+  }
+
+  function endGame(currentClass) {
     return winnersCombnination.some((combinations) => {
       return combinations.every((index) => {
-        return console.log(cellsId[index].classList.contains(currentClass));
-
-        // return cells[index].className.contains(classGame);
+        return cellsId[index].classList.contains(currentClass);
       });
     });
   }
 })();
-// display the player selection
-// function handlclik() {
-//   boardGame.addEventListener('click', (e) => {
-//     let choice = e.target.dataset;
-
-//     if (e.target.dataset) {
-//       cellsId.forEach((cell) => {
-//         if (e.target.textContent == '') {
-//           e.target.textContent = defaultPlayer.input;
-//           if (defaultPlayer == player1) {
-//             e.target.classList.add('player_one');
-//           } else {
-//             e.target.classList.add('player_two');
-//           }
-//           let classGame = e.target.className;
-//           console.log(classGame);
-
-//           switchPlayer();
-//           if (checkwinner(classGame)) {
-//             console.log('yes');
-//           }
-//         } else {
-//           return;
-//         }
-//       });
-//     }
-//   });
-// }
-
-// grid creation
-
-// function boardGrid() {
-//   const arr = [];
-//   const n = 3;
-//   const m = 3;
-//   for (let i = 0; i < n; i++) {
-//     arr.push(new Array(m).fill(null));
-//   }
-
-//   function inputChoice() {
-//     arr.map((el) => {
-//       el.push(defaultPlayer.input);
-//     });
-//     console.log(arr);
-//   }
-//   inputChoice();
-// }
-
-// push the choice of the palyer into the greed
